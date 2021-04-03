92ea6d48d09ee0e03f10378349a8b13c27e0782c
JonaJohn <jona.john@gmx.de>
2021-04-03 19:08:40 +0200

    Lächerlich
    v-bind funktioniert

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var app = new Vue({ // Class
 el: '#app',
 data:{
     product: 'Socks',
-    image: 'C:\Users\John\Desktop\Java.Html.Css\VueJsLearning\images\0.jpg'
+    image: 'C:/Users/John/Desktop/Java.Html.Css/VueJsLearning/images/Auto.jpg'
 }
 
 })
