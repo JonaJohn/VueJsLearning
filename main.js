a30ad6b204ac5818ab4b8aa0181491fd7c6890db
JonaJohn <jona.john@gmx.de>
2021-04-03 21:21:26 +0200

    Funktioniert

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 var app = new Vue({ // Class
 el: '#app',
-data:{
-    product: 'Socks',
-    image: 'C:/Users/John/Desktop/Java.Html.Css/VueJsLearning/images/Auto.jpg'
+data:{//data object
+    product: 'Cars',//property oder Eigenschaften
+    image: 'C:/Users/John/Desktop/Java.Html.Css/VueJsLearning/images/Auto.jpg',
+    link: 'https://de.wikipedia.org/wiki/Adolf_Hitler',
+    inventory:0,
+    onSale: false
 }
 
 })
